bffc69bc489c30a5b6058774828c977fbe498779
ahmadsaadeddins <68161008+ahmadsaadeddins@users.noreply.github.com>
2021-03-05 21:42:23 +0200

    add total

--- core/views.py
+++ core/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Sum, FloatField, F
+
 from .models import Sales
 
 
 def home(request):
     qs = Sales.objects.all()
-    context = {"Sales": qs}
+    qs_count = qs.values('id').annotate(
+        amount=Sum(F('price', ) *
+                   F('qty'), output_field=FloatField())).order_by("-id")
+
+    total = 0
+    for item in qs_count:
+        total += item['amount']
+
+    context = {
+        "Sales": qs,
+        "Total": total,
+    }
     return render(request, "core/home.html", context=context)
