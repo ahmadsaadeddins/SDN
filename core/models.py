4ae273ca0d957f849b7392768a3fbb265aa291e1
ahmadsaadeddins <68161008+ahmadsaadeddins@users.noreply.github.com>
2021-03-05 21:54:06 +0200

    add date and arabic day

--- core/models.py
+++ core/models.py
@@ -1,7 +1,16 @@
+from datetime import datetime
 from django.db import models
+
+from django.utils import translation
+from django.utils.translation import ugettext as _
 
 
 class Sales(models.Model):
+    date = models.DateField(auto_now=True)
+    day = models.CharField(max_length=200,
+                           editable=True,
+                           blank=True,
+                           null=True)
     item_name = models.CharField(max_length=254, verbose_name="اسم المنتج")
     qty = models.PositiveSmallIntegerField(verbose_name="الكمية")
     price = models.DecimalField(verbose_name="السعر",
@@ -14,3 +23,9 @@ class Sales(models.Model):
 
     def __str__(self):
         return self.item_name + " | " + str(self.qty * self.price)
+
+
+def pre_Sales_save_receiver(sender, instance, **kwargs):
+    translation.activate('ar')
+    if not instance.day:
+        instance.day = _(date_format(datetime.today(), 'l'))
