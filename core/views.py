9cbead27e3e670277b8070d3799e995074058ada
ahmadsaadeddins <ahmadsaadeddins@ahm4d.com>
2021-04-03 16:17:30 +0200

    filter by today

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Sum, FloatField, F
-
+from django.utils import timezone
 from .models import Sales
 
 
@@ -8,7 +8,7 @@ def home(request):
     qs = Sales.objects.all()
     qs_count = qs.values('id').annotate(
         amount=Sum(F('price', ) *
-                   F('qty'), output_field=FloatField())).order_by("-id")
+                   F('qty'), output_field=FloatField())).filter(date__gte=timezone.now).order_by("-id")
 
     total = 0
     for item in qs_count:
