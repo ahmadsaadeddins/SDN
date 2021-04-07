83abaf60c6b3b97f3ae6a0b749f6fd3568e50fe4
ahmadsaadeddins <ahmadsaadeddins@ahm4d.com>
2021-04-07 13:25:10 +0200

    total expenses

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ app_name = 'core'
 urlpatterns = [
     path('', coreViews.home, name='home'),
     path('total/', coreViews.total, name='total'),
+    path('totale/', coreViews.total_expenses, name='total-expenses'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Sum, FloatField, F
 from django.utils import timezone
-from .models import Sales
+from .models import Sales, Expenses
 
 
 def home(request):
@@ -23,6 +23,23 @@ def home(request):
 
 def total(request):
     qs = Sales.objects.all().order_by("-id")
+    qs_count = qs.values('id').annotate(
+        amount=Sum(F('price', ) *
+                   F('qty'), output_field=FloatField())).order_by("-id")
+
+    total = 0
+    for item in qs_count:
+        total += item['amount']
+
+    context = {
+        "Sales": qs,
+        "Total": total,
+    }
+    return render(request, "core/home.html", context=context)
+
+
+def total_expenses(request):
+    qs = Expenses.objects.all().order_by("-id")
     qs_count = qs.values('id').annotate(
         amount=Sum(F('price', ) *
                    F('qty'), output_field=FloatField())).order_by("-id")
